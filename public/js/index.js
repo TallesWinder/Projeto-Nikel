0ca262cbf11a8ecae71f58a1801b317a86d8ce7a
Talles Winder <talleswinder@outlook.com>
2022-03-08 17:29:19 -0300

    Adding functions
    Adding functions to save and validate email and password and to register new users.

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,46 @@
 const myModal = new bootstrap.Modal("#register-modal")
 
+//Logar no sistema
+document.getElementById("login-form").addEventListener("submit", function (e) {
+    e.preventDefault()
+
+    const email = document.getElementById("email-input").value
+    const password = document.getElementById("password-input").value
+    const checkSession = document.getElementById("session-check").checked
+
+    const account = getAccount(email)
+
+    if (!account) {
+        alert("Ops! Verifique o usúario ou a senha")
+        return
+    }
+
+    if (account) {
+        if (account.password !== password) {
+            alert("Ops! Verifique o usúario ou a senha")
+            return
+        }
+
+        saveSession(email, checkSession)
+
+        window.location.href = "home.html"
+    }
+
+})
 
 //Criar conta
-document.getElementById("create-form").addEventListener("submit", function(e) {
+document.getElementById("create-form").addEventListener("submit", function (e) {
     e.preventDefault()
 
     const email = document.getElementById("email-create-input").value
     const password = document.getElementById("password-create-input").value
 
-    if(email.length < 5) {
+    if (email.length < 5) {
         alert("Preencha o campo com email válido!")
         return
     }
 
-    if(password.length < 4) {
+    if (password.length < 4) {
         alert("Preencha a senha com no mínimo 4 digitos")
         return
     }
@@ -29,6 +56,31 @@ document.getElementById("create-form").addEventListener("submit", function(e) {
     alert("Conta criada com sucesso!")
 })
 
+////////////////////////////////////////////////////
+
 function saveAccount(data) {
-    localStorage.setItem(data.login,JSON.stringify(data))
+    localStorage.setItem(data.login, JSON.stringify(data))
 }
+
+////////////////////////////////////////////////////
+
+function saveSession(data, saveSession) {
+    if(saveSession) {
+        localStorage.setItem("session", data)
+    }
+
+    sessionStorage.setItem("logged", data)
+}
+
+////////////////////////////////////////////////////
+
+function getAccount(key) {
+    const account = localStorage.getItem(key)
+
+    if (account) {
+        return JSON.parse(account)
+    }
+
+    return ""
+}
+
